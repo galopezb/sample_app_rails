254e4063c4b788ce24a8f1ad511a8ddcceccdc4a
Gustavo López Blandon <gustavo@kommit.co>
2016-10-02 23:31:40 -0500

    Add user following

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,6 +1,6 @@
 require 'test_helper'
 
-class UsersControllerTes < ActionDispatch::IntegrationTest
+class UsersControllerTest < ActionDispatch::IntegrationTest
   def setup
     @user = users(:michael)
     @other_user = users(:archer)
@@ -52,5 +52,15 @@ class UsersControllerTes < ActionDispatch::IntegrationTest
       delete user_path(@user)
     end
     assert_redirected_to root_url
+  end
+
+  test "should redirect following when not logged in" do
+    get following_user_path(@user)
+    assert_redirected_to login_url
+  end
+
+  test "should redirect followers when not logged in" do
+    get followers_user_path(@user)
+    assert_redirected_to login_url
   end
 end
